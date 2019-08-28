168ffe14f7faa84fb1a160995a47c5b6b4465984
DELL <15821038483@163.com>
2019-08-28 21:15:21 +0800

    add token to cookies

--- src/main/java/cn/wasu/community/community/controller/AuthController.java
+++ src/main/java/cn/wasu/community/community/controller/AuthController.java
@@ -13,7 +13,9 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -37,7 +39,7 @@ public class AuthController {
     @GetMapping(value = "callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request, HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(secret);
@@ -53,11 +55,13 @@ public class AuthController {
             User user = new User();
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setName(githubUser.getName());
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setCreateTime(System.currentTimeMillis());
             user.setModifyTime(user.getCreateTime());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
+//            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else {
             //登录失败，返回首页，不显示name
--- src/main/java/cn/wasu/community/community/controller/IndexController.java
+++ src/main/java/cn/wasu/community/community/controller/IndexController.java
@@ -1,14 +1,34 @@
 package cn.wasu.community.community.controller;
 
+import cn.wasu.community.community.mapper.UserMapper;
+import cn.wasu.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie [] cookies=request.getCookies();
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user=userMapper.findByToken( token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
